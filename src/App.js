4022aa50e2af40f58d453f619067780f45ff711f
Andrew <ayjtw7@gmail.com>
2022-09-27 17:56:33 -0400

    initial commit; iterations 1, & 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+
+const contactsArray = contacts.slice(0, 5);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2><u>It works like gold.</u></h2>
+      {contactsArray.map(elem => {
+        return (
+          <div>
+            <table>
+              <ul>
+                <li><img src={elem.pictureUrl} alt={elem.name}></img></li>
+                <li>{elem.name}</li>
+                <li>{elem.popularity}</li>
+              
+                {elem.wonEmmy ? <p>🏆</p> : <p>Not yet</p>}
+              </ul>
+
+
+            </table>
+          </div>
+
+        )
+      })}
+
+
+
+
+
     </div>
   );
 }
